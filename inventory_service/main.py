a4ebea077cba3a1ab5e315e8f5fbf36b528e869a
IrynaHrechyn <90561233+IrynaHrechyn@users.noreply.github.com>
2026-05-10 17:44:38 +0300

    Update and rename main.py to inventory_service/main.py

--- inventory_service/main.py
+++ inventory_service/main.py
@@ -3,10 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Inventory Service 02")
 
-# Персоналізація згідно з Етапом 2 та 3
 STUDENT_N = int(os.getenv("STUDENT_N", 2))
 
-# Початкові ID починаються з 100 * 2 = 200
 inventory_db = {
     201: {"name": "Espresso Machine", "stock": 10},
     202: {"name": "Coffee Beans 1kg", "stock": 50}
@@ -18,7 +16,7 @@ async def get_stock(item_id: int):
     return {
         "item_id": item_id,
         "data": item if item else "Not Found",
-        "student_id": STUDENT_N  # Обов'язкове поле
+        "student_id": STUDENT_N
     }
 
 @app.get("/health")
